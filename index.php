e99fec46684b88c32e2ff0d1169dcf39cbe3b236
Allen Voelcker <58530878+avoelcker@users.noreply.github.com>
2024-01-03 22:10:41 -0500

    Initial codebase
    Initial codebase at start of development.

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+php_require('resources/config.php');
+php_require('resources/header.php');
+php_require('resources/nav.php');
+php_require('resources/content.php');
+php_require('resources/footer.php');
+?>
---
+++ resources/nav.php
@@ -0,0 +1,10 @@
+<nav class="navbar navbar-expand-sm bg-light justify-content-center">
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="index.php">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="add.php">Add Toilet Enemies</a>
+    </li>
+  </ul>
+</nav>
